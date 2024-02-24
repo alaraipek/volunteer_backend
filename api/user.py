f4b5621c1096401ed245cfe344ae98cd3e8c99b6
Tara Sehdave <tara.sehdave@gmail.com>
2024-02-24 13:29:45 -0800

    and again

--- api/user.py
+++ api/user.py
@@ -114,7 +114,7 @@ class UserAPI:
                                 httponly=False,
                                 path='/',
                                 samesite='None',
-                                domain=".github.io"
+                                domain="https://oppconn.stu.nighthawkcodingsociety.com"
                                 )
                         return resp
                     except Exception as e:
